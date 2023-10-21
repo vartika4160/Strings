3a381eb54c7656c538d0e34905f4524f24eb28b5
vartika4160 <139868043+vartika4160@users.noreply.github.com>
2023-10-22 00:06:37 +0530

    Create Concatenated.cpp

---
+++ Concatenated.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+
+int main() {
+    string str1,str2;
+    cout<<"Enter first string: ";
+    getline(cin,str1);
+    cout<<"Enter second string: ";
+    getline(cin,str2);
+    
+    cout<<"Concatenated string is: "<<str1 + str2;
+    
+return 0;
+}
