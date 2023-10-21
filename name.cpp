4d94c5c51c1da56bfd9523949c27538b3899f9d4
vartika4160 <139868043+vartika4160@users.noreply.github.com>
2023-10-22 00:05:11 +0530

    Create name.cpp

---
+++ name.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+
+int main() {
+    string name;
+    cout<<"Enter your name: ";
+    getline(cin,name);
+    cout<<"My name is: "<<name;
+    
+return 0;
+}
